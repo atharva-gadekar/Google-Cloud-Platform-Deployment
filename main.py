adbd6302d2ba52a69397bade349ca69232c5db3f
Atharva Gadekar <97454406+atharva-gadekar@users.noreply.github.com>
2023-10-24 13:08:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template, url_for
+from flask import Flask, request, jsonify, render_template
 import pickle
-
+import ngrok
 
 app = Flask(__name__)
-model = pickle.load(open('randomForestRegressor.pkl','rb'))
+model = pickle.load(open('randomForestRegressor.pkl', 'rb'))
 
+# Set the desired port number for the Flask app
+port_no = 5000
+
+# Input the ngrok authentication token
+ngrok_auth_token = input("Enter your ngrok authentication token: ")
+ngrok.set_auth_token(ngrok_auth_token)
+
+# Connect to ngrok
+public_url = ngrok.connect(port_no).public_url
 
 @app.route('/')
 def home():
-    #return 'Hello World'
     return render_template('home.html')
-    #return render_template('index.html')
 
-@app.route('/predict',methods = ['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
     print(prediction[0])
 
-    #output = round(prediction[0], 2)
     return render_template('home.html', prediction_text="AQI for Jaipur {}".format(prediction[0]))
 
-@app.route('/predict_api',methods=['POST'])
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
-    '''
-    For direct API calls trought request
-    '''
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
 
     output = prediction[0]
     return jsonify(output)
 
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    print(f"To access the Global link, please click {public_url}")
+    app.run(port=port_no, debug=True)
