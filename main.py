3ad3e4b05076bf4a5b218a4929204d3e68ff7774
Atharva Gadekar <97454406+atharva-gadekar@users.noreply.github.com>
2023-10-24 13:12:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-import ngrok
+from pyngrok import ngrok
 
 app = Flask(__name__)
 model = pickle.load(open('randomForestRegressor.pkl', 'rb'))
